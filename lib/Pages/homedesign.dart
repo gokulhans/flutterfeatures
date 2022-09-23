a35ff09e4400a03bda637da3446532a9e95c02ba
gokulhansv <gokulhansv@gmail.com>
2022-09-23 08:17:56 +0530

    new

--- lib/Pages/homedesign.dart
+++ lib/Pages/homedesign.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
@@ -11,10 +12,46 @@ class Homedesign extends StatefulWidget {
 class _HomedesignState extends State<Homedesign> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("home"),
-    );
+    return Scaffold(
+        backgroundColor: Colors.blue[900],
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: const [
+                        Text(
+                          "Hi, Govind!",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.blue[600]),
+                      padding: EdgeInsets.all(4),
+                      child: IconButton(
+                          icon: Icon(
+                            Icons.notifications,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          }),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
-
-
